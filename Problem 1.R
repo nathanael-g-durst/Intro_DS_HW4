337975f1e3ec550a62120a1c9dd6b78ae77259e0
nathanael-g-durst <nathanael.duerst@etu.unige.ch>
2021-05-01 17:28:36 +0200

    Added find_pi

--- Problem 1.R
+++ Problem 1.R
@@ -1,3 +1,62 @@
-library(Rcpp)
-library(RcppArmadillo)
-sourceCpp("rcpp/find_pi.cpp")
+######## [START] Requirements ########
+
+##### Add here the packages needed #############################################
+packagesNeeded <- c("Rcpp", "RcppArmadillo")
+################################################################################
+
+installedPackages <- installed.packages()
+
+for (packageName in packagesNeeded) {
+  packageExists <- is.element(packageName, installedPackages)
+  if(packageExists != TRUE){
+    install.packages(packageName)
+    library(packageName, character.only = TRUE)
+    print(paste(packageName, "has been installed and the library is loaded !"))
+  } else {
+    library(packageName, character.only = TRUE)
+    print(paste(packageName, "is installed and the library is loaded !"))
+  }
+}
+
+rm(installedPackages, packageName, packagesNeeded, packageExists)
+
+##### Add here the external files ##############################################
+sourceCpp("find_pi_rcpp.cpp")
+################################################################################
+
+######## [END] Requirements ########
+
+######## [START] R Functions ########
+
+########## [START] inside_unit_circle ##########
+
+inside_unit_circle = function(x){
+  d = x[1]^2 + x[2]^2
+  (d < 1)
+}
+
+########## [END] inside_unit_circle ##########
+
+########## [START] find_pi ##########
+
+find_pi = function(B = 5000, seed = 10){
+  # Control seed
+  set.seed(seed)
+  
+  # Simulate B points
+  point = matrix(runif(2*B, -1, 1), B, 2)
+  
+  # Compute the number of points inside unit circle
+  nb_inside = apply(point, 1, inside_unit_circle)
+  pi_hat = sum(nb_inside)/B
+  
+  # return estimated value of pi
+  return(4*pi_hat)
+}
+
+########## [END] find_pi ##########
+
+
+
+
+
