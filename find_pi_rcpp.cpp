ad829127726e29f7a2b325c4a0b162a847c57e9b
nathanael-g-durst <nathanael.duerst@etu.unige.ch>
2021-05-03 08:35:09 +0200

    Moving c++ code where it belongs

--- find_pi_rcpp.cpp
+++ find_pi_rcpp.cpp
@@ -12,4 +12,16 @@ double find_pi_cpp(const int B, int seed) {
   return 4.0 * sum(d < 1.0) / B;
 }
 
+double inside_unit_circle(NumericVector x){
+  double d= x[0]*x[0]+x[1]*x[1];
+  return d;
+}
+
+double find_pi_cppp (int B = 5000, int seed = 10){
+  NumericMatrix point(runif(2*B,-1,1),B,2);
+  NumericVector nb_inside = apply_cpp(point,1,inside_unit_circle);
+  double pi_hate =sum(nb_inside)/B;
+  return 4*pi_hate;
+}
+
 //######## [END] C++ Monte Carlo ##########
